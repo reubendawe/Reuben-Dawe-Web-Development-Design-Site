4a6b0b3bfaa21e0a1e507606a33700c5cc05a1dc
Reuben Dawe <reubendawemarketing@gmail.com>
2025-07-31 22:09:19 +0100

    CSS file created inside 'Build' directory

--- wp-content/themes/reuben-dawe-portfolio/functions.php
+++ wp-content/themes/reuben-dawe-portfolio/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function website_files() {
-    wp_enqueue_style('website_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('website_main_styles', get_theme_file_uri('/build/style-index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'website_files');
