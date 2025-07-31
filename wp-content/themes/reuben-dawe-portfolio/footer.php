e93fc966836c2163a4fe5f0f58986829f0a37c7f
Reuben Dawe <reubendawemarketing@gmail.com>
2025-07-31 22:01:51 +0100

    Footer added

--- wp-content/themes/reuben-dawe-portfolio/footer.php
+++ wp-content/themes/reuben-dawe-portfolio/footer.php
@@ -1,3 +1,25 @@
+<footer class="footer">
+    <div class="footer__container container grid">
+        <ul class="footer__links">
+            <li>
+                <a href="#home" class="footer__link">Welcome</a>
+            </li>
+
+            <li>
+                <a href="#about" class="footer__link">Bio</a>
+            </li>
+
+            <li>
+                <a href="#projects" class="footer__link">Portfolio</a>
+            </li>
+        </ul>
+
+        <span class="footer__copy">
+            Website developed by Reuben Dawe (@dawe.js)</a>
+        </span>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
