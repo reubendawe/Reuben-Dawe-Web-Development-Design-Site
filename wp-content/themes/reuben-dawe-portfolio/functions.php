40397e684b52e396c877306f643be8559f9a85e8
Reuben Dawe <reubendawemarketing@gmail.com>
2025-07-31 22:12:23 +0100

    Additional CSS index file created and linked in functions.php

--- wp-content/themes/reuben-dawe-portfolio/functions.php
+++ wp-content/themes/reuben-dawe-portfolio/functions.php
@@ -2,6 +2,7 @@
 
 function website_files() {
     wp_enqueue_style('website_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('website_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'website_files');
