c92e5942076ff19b17b2ace9d7c74d62ea151c35
Reuben Dawe <reubendawemarketing@gmail.com>
2025-09-10 12:52:51 +0100

    Main javascript file added - UI/animations working

--- wp-content/themes/reuben-dawe-portfolio/functions.php
+++ wp-content/themes/reuben-dawe-portfolio/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function website_files() {
+    wp_enqueue_script('main-js', get_theme_file_uri('/build/main.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('remix_icon', '//cdnjs.cloudflare.com/ajax/libs/remixicon/3.4.0/remixicon.css');
     wp_enqueue_style('website_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('website_extra_styles', get_theme_file_uri('/build/index.css'));
